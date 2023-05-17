4faf923d8a42a7ce4169bed8567ceb0772cca804
juanonchon <132412187+juanonchon@users.noreply.github.com>
2023-05-16 21:51:30 -0400

    Add files via upload

---
+++ code.js
@@ -0,0 +1,47 @@
+function factorialN(){
+
+    var N = prompt("Ingrese N", 3)
+    var i = 1
+    var resp = 1
+    if(N >= 0){
+        if(N == 0) {
+           alert("El factorial de N es" + resp)
+        } else {
+            while(N >= i){
+                resp = resp * i
+                i = i + 1
+            }
+            alert("Factorial de N es: " + resp)
+        }   
+    } else {
+        alert("No factorial de un negativo")
+    }
+}
+
+function mayorAyB(){
+    var A = parseIntprompt("ingrese A", 10)
+    var B = parseIntprompt("ingrese B", 7)
+    if(A==B){
+        alert("son iguales")
+    }else{
+        if(A > B){
+            alert("el mayor es A:" + A)
+        }else{
+            alert("el mayor es B:" + B)
+        }
+    }
+}
+
+function menorAyB(){
+    var A = parseIntprompt("ingrese A", 5)
+    var B = parseIntprompt("ingrese B", 7)
+    if(A==B){
+        alert("son iguales")
+    }else{
+        if(A < B){
+            alert("el menor es A:" + A)
+        }else{
+            alert("el menor es B:" + B)
+        }
+    }
+}
